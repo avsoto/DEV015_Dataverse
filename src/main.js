6162d890f9c5b2f52745854f1f71f71cde6a2ce8
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-07-31 18:54:01 -0500

    agregar estructura, estilos y funciones

--- src/main.js
+++ src/main.js
@@ -4,3 +4,18 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+window.onload=function(){
+  const toggleBtn = document.querySelector('.toggle-btn')
+  const btnCloseIcon = document.querySelector('.btn-close i')
+  const dropDownMenu = document.querySelector('.sidebar')
+  
+  toggleBtn.onclick = function(){
+    dropDownMenu.classList.toggle('active')
+  }
+  
+  btnCloseIcon.onclick = function(){
+    dropDownMenu.classList.remove('active')
+  }
+  
+}
