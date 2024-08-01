a94c89f66433a8b19ad7e7bdaed5f7ef7e3c5ee9
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-07-31 23:36:23 -0500

    agregando cambios: estructura, estilos y funciones

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
-
-import data from './data/dataset.js';
-
-console.log(example, renderItems(data), data);
 
 window.onload=function(){
-  const toggleBtn = document.querySelector('.toggle-btn')
+  const menuBtn = document.querySelector('.menu-btn')
   const btnCloseIcon = document.querySelector('.btn-close i')
   const dropDownMenu = document.querySelector('.sidebar')
   
-  toggleBtn.onclick = function(){
+  menuBtn.onclick = function(){
     dropDownMenu.classList.toggle('active')
   }
   
