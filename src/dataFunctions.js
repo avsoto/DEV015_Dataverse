e984d72374b5f59c11582adf34fae4e3b4108f19
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-08-10 01:15:59 -0500

    agregando funciones: filtrado de tipo, género y tamaño, función de botón limpiar

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,3 +1,46 @@
+/* Manipulación de datos */
+import petsData from './data/dataset.js';
+
+const dataFunctions = {
+
+  /* Mostrar Tarjetas */
+  
+  showPets: () => {  
+    if(Array.isArray(petsData)){
+      return petsData;
+    }
+    else{
+      return [];
+    }
+  },
+
+  //------------------ Filtro de Datos ------------------//
+
+  // Filtro Tipo //
+
+  filterDataByType: (petsData,filterBy,value)=> {
+    return petsData.filter(petType => petType[filterBy] === value)
+  },
+
+  //Filtro Género //
+
+  filterDataByGender: (petsData,filterBy,value)=> {
+    return petsData.filter(petGender => petGender.facts[filterBy] === value)
+  },
+
+  //Filtro Tamaño //
+
+  filterDataBySize: (petsData,filterBy,value)=> {
+    return petsData.filter(petSize => petSize.facts[filterBy] === value)
+  },
+
+}
+
+export default dataFunctions;
+
+
+
+
 
 
 
